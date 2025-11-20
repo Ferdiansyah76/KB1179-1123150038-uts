e8d793564fcb5199987588888f0ab9bde8b8b55b
Ferdiansyah76 <bagusferdiansyah750@gmail.com>
2025-11-21 01:10:28 +0700

    membuat file ss3

---
+++ lib/splash_screen/splash_screen_3.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class HibSplashScreen3 extends StatelessWidget {
+  const HibSplashScreen3({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
