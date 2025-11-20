b0792ab77d7273d53fa795f1bdbdb10e694b2e5f
Ferdiansyah76 <bagusferdiansyah750@gmail.com>
2025-11-21 00:19:27 +0700

    membuat file ss2

---
+++ lib/splash_screen/splash_screen_2.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+
+class HibSplashScreen2 extends StatelessWidget {
+  const HibSplashScreen2({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
