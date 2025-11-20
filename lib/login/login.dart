ab22720afbdc726a90fd6207b86b10d89862d0b3
Ferdiansyah76 <bagusferdiansyah750@gmail.com>
2025-11-21 01:35:27 +0700

    membuat file login

---
+++ lib/login/login.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class LoginPageHib extends StatelessWidget {
+  const LoginPageHib({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
