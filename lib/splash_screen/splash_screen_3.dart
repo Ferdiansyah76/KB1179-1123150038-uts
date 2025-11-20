b279bd393a97b090ef55a73836e7d40e67d981c1
Ferdiansyah76 <bagusferdiansyah750@gmail.com>
2025-11-21 01:20:00 +0700

    menambahkan gambar di ss3

--- lib/splash_screen/splash_screen_3.dart
+++ lib/splash_screen/splash_screen_3.dart
@@ -7,7 +7,27 @@ class HibSplashScreen3 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Spacer(),
+            SizedBox(height: 20),
+            Container(
+              height: 220,
+              width: 220,
+              decoration: BoxDecoration(
+                shape: BoxShape.circle,
+                color: Colors.transparent,
+                image: DecorationImage(
+                  image: AssetImage("/hibbrione.png"),
+                  fit: BoxFit.cover,
+                ),
+              ),
+            ),
+          ]
+        ),
+      ),
     );
   }
 }
