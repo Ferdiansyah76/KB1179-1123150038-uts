3bed7b843922579c7b3c8a78ba21f3e379038be0
Ferdiansyah76 <bagusferdiansyah750@gmail.com>
2025-11-21 01:51:05 +0700

    penambahan 2 taxt pada tampilan login

--- lib/login/login.dart
+++ lib/login/login.dart
@@ -6,7 +6,33 @@ class LoginPageHib extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Center(
+        child: SingleChildScrollView(
+          child: Container(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                const SizedBox(height: 10),
+                const Icon(Icons.lock_outline, size: 80, color: Colors.green),
+                const SizedBox(height: 20),
+                const Text(
+                  "Selamat Datang!",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                const Text(
+                  "Silahkan Login untuk melanjutkan.",
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 12, color: Colors.green),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
